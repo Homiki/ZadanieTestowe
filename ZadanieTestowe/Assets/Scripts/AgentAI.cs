84d1e523858578f15b76d25520f8531ed0fbbc2d
Mikołaj Klekotka <HomikiGaming@gmail.com>
2023-02-21 18:16:24 +0100

    need to add speed of agent

--- ZadanieTestowe/Assets/Scripts/AgentAI.cs
+++ ZadanieTestowe/Assets/Scripts/AgentAI.cs
@@ -6,6 +6,10 @@ using UnityEngine.AI;
 public class AgentAI : MonoBehaviour
 {
     NavMeshAgent agent;
+
+    public float range;
+
+    public Transform centrePoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,28 @@ public class AgentAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            Vector3 point;
+            if(RandomPoint(centrePoint.position, range, out point))
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                agent.SetDestination(point);
+            }
+        }
+    }
+
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
     }
 }
