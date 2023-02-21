b4502b808f74eeddc8957516adaaa23e35ee5dd9
Mikołaj Klekotka <HomikiGaming@gmail.com>
2023-02-21 19:59:48 +0100

    Testing Spawner

--- ZadanieTestowe/Assets/Scripts/AgentSpawner.cs
+++ ZadanieTestowe/Assets/Scripts/AgentSpawner.cs
@@ -15,15 +15,34 @@ public class AgentSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 1; i <= startingAgent; i++)
+        {
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-47, 48), 5.1f, Random.Range(-47, 48));
+            Instantiate(agentObject, randomSpawnPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //for(int i = 0 ; i <= maxAgent; i++)
+        //{
+        //    StartCoroutine(Waiter());
+        //    Vector3 randomSpawnPosition = new Vector3(Random.Range(-47, 48), 5.1f, Random.Range(-47, 48));
+        //    Instantiate(agentObject, randomSpawnPosition, Quaternion.identity);
+        //}
+        StartCoroutine(Waiter());
+    }
 
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(-47, 48), 5.1f, Random.Range(-47, 48));
-        Instantiate(agentObject, randomSpawnPosition, Quaternion.identity);
+    IEnumerator Waiter()
+    {
+        //yield return new WaitForSeconds(spawningTime);
 
+        for (int i = 0; i <= maxAgent; i++)
+        {
+            yield return new WaitForSeconds(spawningTime);
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-47, 48), 5.1f, Random.Range(-47, 48));
+            Instantiate(agentObject, randomSpawnPosition, Quaternion.identity);
+        }
     }
 }
